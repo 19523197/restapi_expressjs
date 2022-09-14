5228672f78cd53c8df26a039f0a87ee280f88cae
19523197 <19523197@students.uii.ac.id>
2022-09-14 11:13:52 +0700

    tes heroku database connect

--- db.config.js
+++ db.config.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
     const pool = new Pool({
-        host : "localhost",
-        user : "postgres",
-        password : "magang123",
-        database : "magangapp",
+        host : "ec2-44-209-158-64.compute-1.amazonaws.com",
+        user : "qputtgyidacyqb",
+        password : "ccfc6f3825ec073d84afdfae541a74add21a1bbd3de0b2375ca305572fd663b4",
+        database : "d2ceegb96r1rib",
        
 
     })
